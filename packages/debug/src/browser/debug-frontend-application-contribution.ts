f01982ab6c506700187cbfd6ffb9fe6d5c7741e6
vince-fugnitto <vincent.fugnitto@ericsson.com>
2022-08-18 17:12:34 -0400

    vscode: add `hcLight` support
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- packages/debug/src/browser/debug-frontend-application-contribution.ts
+++ packages/debug/src/browser/debug-frontend-application-contribution.ts
@@ -1229,7 +1229,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 defaults: {
                     dark: '#ffff0033',
                     light: '#ffff6673',
-                    hc: '#fff600'
+                    hcDark: '#fff600',
+                    hcLight: '#ffff6673'
                 }, description: 'Background color for the highlight of line at the top stack frame position.'
             },
             {
@@ -1237,7 +1238,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 defaults: {
                     dark: '#7abd7a4d',
                     light: '#cee7ce73',
-                    hc: '#cee7ce'
+                    hcDark: '#cee7ce',
+                    hcLight: '#cee7ce73'
                 }, description: 'Background color for the highlight of line at focused stack frame position.'
             },
             // Status bar colors should be aligned with debugging colors from https://code.visualstudio.com/api/references/theme-color#status-bar-colors
@@ -1245,21 +1247,24 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'statusBar.debuggingBackground', defaults: {
                     dark: '#CC6633',
                     light: '#CC6633',
-                    hc: '#CC6633'
+                    hcDark: '#CC6633',
+                    hcLight: '#B5200D'
                 }, description: 'Status bar background color when a program is being debugged. The status bar is shown in the bottom of the window'
             },
             {
                 id: 'statusBar.debuggingForeground', defaults: {
                     dark: 'statusBar.foreground',
                     light: 'statusBar.foreground',
-                    hc: 'statusBar.foreground'
+                    hcDark: 'statusBar.foreground',
+                    hcLight: 'statusBar.foreground'
                 }, description: 'Status bar foreground color when a program is being debugged. The status bar is shown in the bottom of the window'
             },
             {
                 id: 'statusBar.debuggingBorder', defaults: {
                     dark: 'statusBar.border',
                     light: 'statusBar.border',
-                    hc: 'statusBar.border'
+                    hcDark: 'statusBar.border',
+                    hcLight: 'statusBar.border'
                 }, description: 'Status bar border color separating to the sidebar and editor when a program is being debugged. The status bar is shown in the bottom of the window'
             },
             // Debug Exception Widget colors should be aligned with
@@ -1268,14 +1273,16 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugExceptionWidget.border', defaults: {
                     dark: '#a31515',
                     light: '#a31515',
-                    hc: '#a31515'
+                    hcDark: '#a31515',
+                    hcLight: '#a31515'
                 }, description: 'Exception widget border color.',
             },
             {
                 id: 'debugExceptionWidget.background', defaults: {
                     dark: '#420b0d',
                     light: '#f1dfde',
-                    hc: '#420b0d'
+                    hcDark: '#420b0d',
+                    hcLight: '#f1dfde'
                 }, description: 'Exception widget background color.'
             },
             // Debug Icon colors should be aligned with
@@ -1284,7 +1291,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugIcon.breakpointForeground', defaults: {
                     dark: '#E51400',
                     light: '#E51400',
-                    hc: '#E51400'
+                    hcDark: '#E51400',
+                    hcLight: '#E51400'
                 },
                 description: 'Icon color for breakpoints.'
             },
@@ -1292,7 +1300,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugIcon.breakpointDisabledForeground', defaults: {
                     dark: '#848484',
                     light: '#848484',
-                    hc: '#848484'
+                    hcDark: '#848484',
+                    hcLight: '#848484'
                 },
                 description: 'Icon color for disabled breakpoints.'
             },
@@ -1300,15 +1309,17 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugIcon.breakpointUnverifiedForeground', defaults: {
                     dark: '#848484',
                     light: '#848484',
-                    hc: '#848484'
+                    hcDark: '#848484',
+                    hcLight: '#848484'
                 },
                 description: 'Icon color for unverified breakpoints.'
             },
             {
                 id: 'debugIcon.breakpointCurrentStackframeForeground', defaults: {
                     dark: '#FFCC00',
-                    light: '#FFCC00',
-                    hc: '#FFCC00'
+                    light: '#BE8700',
+                    hcDark: '#FFCC00',
+                    hcLight: '#BE8700'
                 },
                 description: 'Icon color for the current breakpoint stack frame.'
             },
@@ -1316,7 +1327,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugIcon.breakpointStackframeForeground', defaults: {
                     dark: '#89D185',
                     light: '#89D185',
-                    hc: '#89D185'
+                    hcDark: '#89D185',
+                    hcLight: '#89D185'
                 },
                 description: 'Icon color for all breakpoint stack frames.'
             },
@@ -1324,84 +1336,96 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugIcon.startForeground', defaults: {
                     dark: '#89D185',
                     light: '#388A34',
-                    hc: '#89D185'
+                    hcDark: '#89D185',
+                    hcLight: '#388A34'
                 }, description: 'Debug toolbar icon for start debugging.'
             },
             {
                 id: 'debugIcon.pauseForeground', defaults: {
                     dark: '#75BEFF',
                     light: '#007ACC',
-                    hc: '#75BEFF'
+                    hcDark: '#75BEFF',
+                    hcLight: '#007ACC'
                 }, description: 'Debug toolbar icon for pause.'
             },
             {
                 id: 'debugIcon.stopForeground', defaults: {
                     dark: '#F48771',
                     light: '#A1260D',
-                    hc: '#F48771'
+                    hcDark: '#F48771',
+                    hcLight: '#A1260D'
                 }, description: 'Debug toolbar icon for stop.'
             },
             {
                 id: 'debugIcon.disconnectForeground', defaults: {
                     dark: '#F48771',
                     light: '#A1260D',
-                    hc: '#F48771'
+                    hcDark: '#F48771',
+                    hcLight: '#A1260D'
                 }, description: 'Debug toolbar icon for disconnect.'
             },
             {
                 id: 'debugIcon.restartForeground', defaults: {
                     dark: '#89D185',
                     light: '#388A34',
-                    hc: '#89D185'
+                    hcDark: '#89D185',
+                    hcLight: '#388A34'
                 }, description: 'Debug toolbar icon for restart.'
             },
             {
                 id: 'debugIcon.stepOverForeground', defaults: {
                     dark: '#75BEFF',
                     light: '#007ACC',
-                    hc: '#75BEFF'
+                    hcDark: '#75BEFF',
+                    hcLight: '#007ACC',
                 }, description: 'Debug toolbar icon for step over.'
             },
             {
                 id: 'debugIcon.stepIntoForeground', defaults: {
                     dark: '#75BEFF',
                     light: '#007ACC',
-                    hc: '#75BEFF'
+                    hcDark: '#75BEFF',
+                    hcLight: '#007ACC'
                 }, description: 'Debug toolbar icon for step into.'
             },
             {
                 id: 'debugIcon.stepOutForeground', defaults: {
                     dark: '#75BEFF',
                     light: '#007ACC',
-                    hc: '#75BEFF'
+                    hcDark: '#75BEFF',
+                    hcLight: '#007ACC',
                 }, description: 'Debug toolbar icon for step over.'
             },
             {
                 id: 'debugIcon.continueForeground', defaults: {
                     dark: '#75BEFF',
                     light: '#007ACC',
-                    hc: '#75BEFF'
+                    hcDark: '#75BEFF',
+                    hcLight: '#007ACC'
                 }, description: 'Debug toolbar icon for continue.'
             },
             {
                 id: 'debugIcon.stepBackForeground', defaults: {
                     dark: '#75BEFF',
                     light: '#007ACC',
-                    hc: '#75BEFF'
+                    hcDark: '#75BEFF',
+                    hcLight: '#007ACC'
                 }, description: 'Debug toolbar icon for step back.'
             },
             {
                 id: 'debugConsole.infoForeground', defaults: {
                     dark: 'editorInfo.foreground',
                     light: 'editorInfo.foreground',
-                    hc: 'foreground'
+                    hcDark: 'foreground',
+                    hcLight: 'foreground'
                 }, description: 'Foreground color for info messages in debug REPL console.'
             },
             {
                 id: 'debugConsole.warningForeground', defaults: {
                     dark: 'editorWarning.foreground',
                     light: 'editorWarning.foreground',
-                    hc: '#008000'
+                    hcDark: '#008000',
+                    hcLight: 'editorWarning.foreground'
                 },
                 description: 'Foreground color for warning messages in debug REPL console.'
             },
@@ -1409,7 +1433,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugConsole.errorForeground', defaults: {
                     dark: 'errorForeground',
                     light: 'errorForeground',
-                    hc: 'errorForeground'
+                    hcDark: 'errorForeground',
+                    hcLight: 'errorForeground'
                 },
                 description: 'Foreground color for error messages in debug REPL console.',
             },
@@ -1417,7 +1442,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugConsole.sourceForeground', defaults: {
                     dark: 'foreground',
                     light: 'foreground',
-                    hc: 'foreground'
+                    hcDark: 'foreground',
+                    hcLight: 'foreground'
                 },
                 description: 'Foreground color for source filenames in debug REPL console.',
             },
@@ -1425,7 +1451,8 @@ export class DebugFrontendApplicationContribution extends AbstractViewContributi
                 id: 'debugConsoleInputIcon.foreground', defaults: {
                     dark: 'foreground',
                     light: 'foreground',
-                    hc: 'foreground'
+                    hcDark: 'foreground',
+                    hcLight: 'foreground'
                 },
                 description: 'Foreground color for debug console input marker icon.'
             }
